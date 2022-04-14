11cc317dcecccb6b19a0aad3fb2d750b9eb38e86
Prihler <94796499+Prihler@users.noreply.github.com>
2022-04-14 22:32:27 +0200

    Update umi.sh

--- umi.sh
+++ umi.sh
@@ -11,12 +11,12 @@ makepkg -si --noconfirm
 cd ..
 
 # Installing Awesome
-paru -S xorg awesome --noconfirm
-mkdir -p ~/.config/awesome/
-cd ~/.config/awesome/
-wget https://raw.githubusercontent.com/Prihler/dotfiles/main/rc.lua
-wget https://raw.githubusercontent.com/Prihler/dotfiles/main/autorun.sh
-chmod +x autorun.sh
+#paru -S xorg awesome --noconfirm
+#mkdir -p ~/.config/awesome/
+#cd ~/.config/awesome/
+#wget https://raw.githubusercontent.com/Prihler/dotfiles/main/rc.lua
+#wget https://raw.githubusercontent.com/Prihler/dotfiles/main/autorun.sh
+#chmod +x autorun.sh
 
 
 
