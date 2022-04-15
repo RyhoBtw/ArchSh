0f05bd30abae7c57bfdee59225ad306c8c767d52
Prihler <94796499+Prihler@users.noreply.github.com>
2022-04-15 19:12:23 +0200

    Update umi.sh

--- umi.sh
+++ umi.sh
@@ -4,17 +4,17 @@ sudo pacman -Syu --noconfirm
 
 sudo pacman -S base-devel git wget --needed --noconfirm
 
-#Installing Paru
+# Installing Paru
 git clone https://aur.archlinux.org/paru.git
 cd paru
 makepkg -si --noconfirm
 cd ..
 
 # Installing Awesome
-#paru -S xorg awesome --noconfirm
-#mkdir -p ~/.config/awesome/
-#cd ~/.config/awesome/
-#wget https://raw.githubusercontent.com/Prihler/dotfiles/main/rc.lua
+paru -S awesome --noconfirm
+mkdir -p ~/.config/awesome/
+cd ~/.config/awesome/
+wget https://raw.githubusercontent.com/Prihler/dotfiles/main/rc.lua
 #wget https://raw.githubusercontent.com/Prihler/dotfiles/main/autorun.sh
 #chmod +x autorun.sh
 
