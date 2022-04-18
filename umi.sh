026dab7bd087e03c55eeab3cdedfb7560ad5a53f
Prihler <94796499+Prihler@users.noreply.github.com>
2022-04-18 13:54:06 +0200

    Update umi.sh

--- umi.sh
+++ umi.sh
@@ -51,7 +51,7 @@ cd ~
 #paru -S lf --noconfirm
 
 # Installing fonts
-paru -S ttf-ms-fonts --noconfirm
+#paru -S ttf-ms-fonts --noconfirm
 paru -S nerd-fonts-mononok --noconfirm
 paru -S nerd-fonts-jetbrains-mono --noconfirm
 
