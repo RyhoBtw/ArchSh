085bf4f043a2cfb35072619d2ff670e55cd21efc
Prihler <94796499+Prihler@users.noreply.github.com>
2022-04-21 15:07:34 +0200

    Update umi.sh

--- umi.sh
+++ umi.sh
@@ -21,10 +21,10 @@ xdg-user-dirs-update
 sudo pacman -S base-devel git --needed --noconfirm
 
 # Installing Paru
-git clone https://aur.archlinux.org/paru.git
-cd paru
-makepkg -si --noconfirm
-cd ~
+#git clone https://aur.archlinux.org/paru.git
+#cd paru
+#makepkg -si --noconfirm
+#cd ~
 
 
 
