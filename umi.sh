b6ce5a095e7a2b67b70b05472aea7581f1058ba8
Prihler <94796499+Prihler@users.noreply.github.com>
2022-04-21 13:23:48 +0200

    Update umi.sh

--- umi.sh
+++ umi.sh
@@ -11,60 +11,59 @@ cd ~
 sudo pacman -Syu --noconfirm
 
 # xdg-user-dirs
-#sudo pacman -S xdg-user-dirs --noconfirm
-#cd ~/.config
-#curl -LO https://raw.githubusercontent.com/Prihler/dotfiles/main/user-dirs.dirs
-#cd ~
-#xdg-user-dirs-update
+sudo pacman -S xdg-user-dirs --noconfirm
+cd ~/.config
+curl -LO https://raw.githubusercontent.com/Prihler/dotfiles/main/user-dirs.dirs
+cd ~
+xdg-user-dirs-update
 
 # Installing needed packages for the script
-#sudo pacman -S base-devel git --needed --noconfirm
+sudo pacman -S base-devel git --needed --noconfirm
 
 # Installing Paru
-#git clone https://aur.archlinux.org/paru.git
-#cd paru
-#makepkg -si --noconfirm
-#cd ~
+git clone https://aur.archlinux.org/paru.git
+cd paru
+makepkg -si --noconfirm
+cd ~
 
 
 
 # changing bash-files location
-#mkdir -p ~/.config/bash/
-#mkdir -p ~/.cache/bash/
-#mv ~/.bash_history ~/.cache/bash/
-#mv ~/.bash_logout ~/.cache/bash/
-#mv ~/.bash_profile ~/.config/bash/
-#mv ~/.bashrc ~/.config/bash/
+mkdir -p ~/.config/bash/
+mkdir -p ~/.cache/bash/
+mv ~/.bash_history ~/.cache/bash/
+mv ~/.bash_logout ~/.cache/bash/
+mv ~/.bash_profile ~/.config/bash/
+mv ~/.bashrc ~/.config/bash/
 
 # Installing dash
-#sudo pacman -S dash --noconfirm
-#sudo ln -sfT /bin/dash /bin/sh
-#cd /usr/share/libalpm/hooks/
-#sudo curl -LO https://raw.githubusercontent.com/Prihler/dotfiles/main/bash-update.hook
-#cd ~
+sudo pacman -S dash --noconfirm
+sudo ln -sfT /bin/dash /bin/sh
+cd /usr/share/libalpm/hooks/
+sudo curl -LO https://raw.githubusercontent.com/Prihler/dotfiles/main/bash-update.hook
+cd ~
 
 # Installing zsh
-#paru -S zsh zsh-syntax-highlighting zsh-theme-powerlevel10k-git --noconfirm
-#mkdir -p ~/.cache/zsh
-#curl -LO https://raw.githubusercontent.com/Prihler/dotfiles/main/.zshenv
-#mkdir -p ~/.config/zsh/
-#cd ~/.config/zsh
-#curl -LO https://raw.githubusercontent.com/Prihler/dotfiles/main/.zshrc
-#curl -LO https://raw.githubusercontent.com/Prihler/dotfiles/main/.p10k.zsh
-#cd ~
+paru -S zsh zsh-syntax-highlighting zsh-theme-powerlevel10k-git --noconfirm
+mkdir -p ~/.cache/zsh
+curl -LO https://raw.githubusercontent.com/Prihler/dotfiles/main/.zshenv
+mkdir -p ~/.config/zsh/
+cd ~/.config/zsh
+curl -LO https://raw.githubusercontent.com/Prihler/dotfiles/main/.zshrc
+curl -LO https://raw.githubusercontent.com/Prihler/dotfiles/main/.p10k.zsh
+cd ~
 
 
 # aliases
-#cd ~/.config
-#curl -LO https://raw.githubusercontent.com/Prihler/dotfiles/main/aliasrc
+cd ~/.config
+curl -LO https://raw.githubusercontent.com/Prihler/dotfiles/main/aliasrc
 
 # Alacritty config & Picom
-#sudo pacman -S picom --noconfirm
-#mkdir -p ~/.config/alacritty/
-#cd ~/.config/alacritty/
-#curl -LO https://raw.githubusercontent.com/Prihler/dotfiles/main/alacritty.yml
-#cd ~
-
+sudo pacman -S picom --noconfirm
+mkdir -p ~/.config/alacritty/
+cd ~/.config/alacritty/
+curl -LO https://raw.githubusercontent.com/Prihler/dotfiles/main/alacritty.yml
+cd ~
 
 # Awesome config
 mkdir -p ~/.config/awesome/
@@ -82,14 +81,14 @@ paru -S nitrogen --noconfirm
 
 # Installing fonts
 #paru -S ttf-ms-fonts --noconfirm
-#paru -S nerd-fonts-mononok --noconfirm
-#paru -S nerd-fonts-jetbrains-mono --noconfirm
+paru -S nerd-fonts-mononok --noconfirm
+paru -S nerd-fonts-jetbrains-mono --noconfirm
 
 # removing unwanted packages
-#paru -R xterm --noconfirm
+paru -R xterm --noconfirm
 
 # Installing packages
-#paru -S lf neovim man fzf --noconfirm
+paru -S lf neovim man fzf --noconfirm
 
 
 # removing the script
