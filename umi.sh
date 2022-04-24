02d47879dec65c0467f7a5228ddac24c90703f8d
Prihler <94796499+Prihler@users.noreply.github.com>
2022-04-24 18:35:41 +0200

    Update umi.sh

--- umi.sh
+++ umi.sh
@@ -86,8 +86,11 @@ cd ~
 paru -S nitrogen --noconfirm
 mkdir ~/Pictures/wallpaper
 cd ~/Pictures/wallpaper
-curl -LO https://i.redd.it/6se14k41od671.png
-mv 6se14k41od671.png doge.png
+#curl -LO https://i.redd.it/6se14k41od671.png
+#curl -LO https://i.redd.it/z0bug4fwyhc51.png
+#mv 6se14k41od671.png doge.png
+#mv z0bug4fwyhc51.png forest-mountain.png
+git clone https://gitlab.com/Prihler/wallpapers.git
 cd  ~
 
 # Installing ly
