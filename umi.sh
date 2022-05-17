843399614b687f579f4d999b46147d17dfa593d0
Prihler <Prihler@users.noreply.github.com>
2022-05-17 20:58:47 +0200

    Update umi.sh

--- umi.sh
+++ umi.sh
@@ -109,13 +109,12 @@ sudo rm /usr/share/lightdm-webkit/themes/lightdm-webkit-theme-aether/src/img/log
 sudo cp ~/umi-tmp/Arch-white.png /usr/share/lightdm-webkit/themes/lightdm-webkit-theme-aether/src/img/logos/
 sudo rm /usr/share/lightdm-webkit/themes/lightdm-webkit-theme-aether/src/img/wallpapers/*
 sudo cp ~/umi-tmp/lightdm-forest.jpg /usr/share/lightdm-webkit/themes/lightdm-webkit-theme-aether/src/img/wallpapers/
-#rm -r /var/lib/lightdm/.local/share/webkitgtk/localstorage
 #sudo mkdir /var/lib/lightdm/.local/share/webkitgtk/
 sudo mkdir /opt/scripts/
 sudo touch /opt/scripts/aether-theme-setup
 sudo chmod +x /opt/scripts/aether-theme-setup
 cd /opt/scripts/
-sudo curl -LO https://raw.githubusercontent.com/Prihler/dotfiles/main/aetger
+sudo curl -LO https://raw.githubusercontent.com/Prihler/dotfiles/main/aether-theme-setup
 #sudo cp -r ~/umi-tmp/localstorage /var/lib/lightdm/.local/share/webkitgtk/
 #rm -r ~/umi-tmp
 cd ~
