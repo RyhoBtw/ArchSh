897d7c2876513799f974d7ab177d75e1c94c3388
Prihler <94796499+Prihler@users.noreply.github.com>
2022-06-25 14:29:03 +0200

    .

--- programms.sh
+++ programms.sh
@@ -6,7 +6,7 @@ paru -Syu --noconfirm
 paru -S discord --noconfirm
 
 # Installing VM
-paru -S qemu libvirt virt-manager lxsession dnsmashq --noconfirm     #ebtables
+paru -S qemu libvirt virt-manager lxsession dnsmasq --noconfirm     #ebtables
 sudo systemctl enable libvirtd
 sudo usermod -G libvirt -a $USER
 echo 'sudo virsh net-start default' | sudo tee -a /opt/scripts/kvm-network.sh
