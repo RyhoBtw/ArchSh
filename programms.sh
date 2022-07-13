2a6a2125cac5fcf0e1f6ed4de0c09881ae5f767f
prihler <94796499+Prihler@users.noreply.github.com>
2022-07-13 22:43:42 +0200

    Update programms.sh

--- programms.sh
+++ programms.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+#xrandr --output DP-0 --mode 1920x1080 --pos 0x0 --output DP-2 --primary --mode 1920x1080 --pos 1920x0 --output DP-4 --mode 1920x1080 --pos 3840x0
+
 # Updating system
 paru -Syu --noconfirm
 
@@ -11,8 +13,5 @@ sudo systemctl enable libvirtd
 sudo usermod -G libvirt -a $USER
 echo 'sudo virsh net-start default' | sudo tee -a /opt/scripts/kvm-network.sh
 sudo chmod +x /opt/scripts/kvm-network.sh
-
-
-#xrandr --output DVI-D-0 --off --output HDMI-0 --off --output DP-0 --mode 1920x1080 --pos 0x0 --rotate normal --output DP-1 --off --output DP-2 --primary --mode 1920x1080 --pos 1920x0 --rotate normal --output DP-3 --off --output DP-4 --mode 1920x1080 --pos 3840x0 --rotate normal --output DP-5 --off
 
 rm programms.sh
