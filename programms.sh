1367605103647e8e5bd519e80c1ff6eb87d057f0
prihler <94796499+Prihler@users.noreply.github.com>
2022-07-17 16:59:06 +0200

    Update programms.sh

--- programms.sh
+++ programms.sh
@@ -11,8 +11,8 @@ paru -S discord bitwarden spotify github-desktop nvidia-settings --noconfirm
 paru -S qemu libvirt virt-manager lxsession dnsmasq --noconfirm     #ebtables
 sudo systemctl enable libvirtd
 sudo usermod -G libvirt -a $USER
-echo 'sudo virsh net-start default' | sudo tee -a /opt/scripts/kvm-network.sh
-sudo chmod +x /opt/scripts/kvm-network.sh
+echo 'sudo virsh net-start default' | sudo tee -a /opt/kvm-network.sh
+sudo chmod +x /opt/kvm-network.sh
 
 # steam
 
