bd0b70b7f7a69f9a02acd5e1d011cd70f7588070
Prihler <94796499+Prihler@users.noreply.github.com>
2022-08-04 23:23:57 +0200

    Update after-rice.sh

--- after-rice.sh
+++ after-rice.sh
@@ -8,11 +8,12 @@ paru -S protonup-qt --noconfirm
 sudo nvidia-settings
 
 # Librewolf addons
-librewolf --new-tab addons.mozilla.org/en-US/firefox/addon/decentraleyes/
-librewolf --new-tab addons.mozilla.org/en-US/firefox/addon/clearurls/
-librewolf --new-tab addons.mozilla.org/en-US/firefox/addon/return-youtube-dislikes/
-librewolf --new-tab addons.mozilla.org/en-US/firefox/addon/sponsorblock/
+librewolf --new-tab addons.mozilla.org/en-US/firefox/addon/decentraleyes/ &
+librewolf --new-tab addons.mozilla.org/en-US/firefox/addon/clearurls/ &
+librewolf --new-tab addons.mozilla.org/en-US/firefox/addon/return-youtube-dislikes/ &
+librewolf --new-tab addons.mozilla.org/en-US/firefox/addon/sponsorblock/ &
 
 # removing the script
 sed -i "/after-rice.sh/d" ~/.config/awesome/rc.lua
 sudo rm /opt/after-rice.sh
+kill -25 $PPID
