a2ea8e18e51a3b0a29d9b99f6903e84a8bc99896
Prihler <94796499+Prihler@users.noreply.github.com>
2022-08-04 18:32:30 +0200

    Update umi.sh

--- umi.sh
+++ umi.sh
@@ -28,6 +28,7 @@ sudo pacman-key --populate archlinux
 mkdir $HOME/.config
 sudo pacman -S xdg-user-dirs --noconfirm
 cd $HOME/.config && curl -LO https://raw.githubusercontent.com/Prihler/dotfiles/main/user-dirs.dirs && cd $HOME
+echo "------------------------------------"
 sleep 10
 mkdir $HOME/downloads
 mkdir $HOME/public
