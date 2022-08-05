0fc7cec8742dad983bc202af5c7a5823037a04af
Prihler <94796499+Prihler@users.noreply.github.com>
2022-08-05 13:55:11 +0200

    Update after-rice.sh

--- after-rice.sh
+++ after-rice.sh
@@ -16,7 +16,11 @@ sleep 10
 librewolf --new-tab addons.mozilla.org/en-US/firefox/addon/clearurls/ &
 librewolf --new-tab addons.mozilla.org/en-US/firefox/addon/return-youtube-dislikes/ &
 librewolf --new-tab addons.mozilla.org/en-US/firefox/addon/sponsorblock/ &
-librewolf --new-tab addons.mozilla.org/en-US/firefox/addon/canvasblocker/ &
+librewolf --new-tab addons.mozilla.org/en-US/firefox/addon/canvasblocker/ 
+
+clear
+echo "Press Enter to finish"
+read
 
 notify-send "Setup done"
 
