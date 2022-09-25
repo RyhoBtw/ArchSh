26fbf9110c48cc39d5cf994fcaa35299ddb25fe9
Prihler <94796499+Prihler@users.noreply.github.com>
2022-09-25 10:37:50 +0000

    Update after-rice.sh

--- after-rice.sh
+++ after-rice.sh
@@ -8,9 +8,6 @@ paru -S protonup-qt --noconfirm
 # Nvidia settings
 sudo nvidia-settings
 
-# Setting up Discord
-echo 'awful.spawn.with_shell("discord")' >> /$HOME/.config/awesome/rc.lua
-
 # removing the script
 sed -i "/after-rice.sh/d" ~/.config/awesome/rc.lua
 sudo rm /opt/after-rice.sh
