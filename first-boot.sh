8d9115266c8d8433f1336d4d56e66de04b899dae
RyhoBtw <114360068+RyhoBtw@users.noreply.github.com>
2022-11-15 17:04:30 +0000

    Update first-boot.sh

--- first-boot.sh
+++ first-boot.sh
@@ -1,23 +1,9 @@
 #!/bin/sh
 #sudo sed -i 's/^#MAKEFLAGS=.*/MAKEFLAGS="-j256"/g' /etc/makepkg.conf
 
-# Setting up steam
-paru -S steam
-paru -S protonup-qt --noconfirm
-
 # Nvidia settings
-# sudo nvidia-settings
+sudo nvidia-settings
 
 # removing the script
-sed -i "/first-boot.sh/d" ~/.config/awesome/rc.lua
+sed -i "$(( $(wc -l <$HOME/.config/awesome/rc.lua)-3+1 )),$ d" $HOME/.config/awesome/rc.lua
 sudo rm /opt/first-boot.sh
-
-# Librewolf addons
-#librewolf --new-tab addons.mozilla.org/en-US/firefox/addon/decentraleyes/ &
-#sleep 5
-#librewolf addons.mozilla.org/en-US/firefox/addon/clearurls/ &
-#librewolf addons.mozilla.org/en-US/firefox/addon/return-youtube-dislikes/ &
-#librewolf addons.mozilla.org/en-US/firefox/addon/sponsorblock/ &
-#librewolf addons.mozilla.org/en-US/firefox/addon/canvasblocker/
-#librewolf https://addons.mozilla.org/en-US/firefox/addon/user-agent-string-switcher/
-#librewolf https://addons.mozilla.org/en-US/firefox/addon/df-youtube/
